202472f0c595dbbf30e875c301689b6f3999613f
C#Developer <shahzodkiller11@gmail.com>
2020-04-19 18:39:07 +0500

    Complete

--- Program.cs
+++ Program.cs
@@ -1,125 +1,75 @@
 ﻿using System;
-using System.Data.SqlClient;
-using System.Data;
-
 namespace HW
 {
     class Program
     {
-        static void Main()
+        delegate T Operation<T>(T x, T y);
+        static void Main(string[] args)
         {
-            ArrayHelper<string> functionList = new ArrayHelper<string>();
-            string[] words = {"horse","dog","Elephant","Payrav","Akmal","MadMax"};
-            functionList.Slice(ref words, 1, -3);
-            foreach(var item in words)
+            bool work = true;
+            while (work == true)
             {
-                System.Console.Write(item+",");
+                Console.ForegroundColor = ConsoleColor.Green;
+                System.Console.WriteLine("Operations:");
+                System.Console.WriteLine("+");
+                System.Console.WriteLine("-");
+                System.Console.WriteLine("*");
+                System.Console.WriteLine("/");
+                System.Console.WriteLine("quit");
+                Console.ForegroundColor = ConsoleColor.White;
+                System.Console.Write("Operation:");
+                string operation = Console.ReadLine();
+                Console.Clear();
+                if (operation != "quit")
+                {
+                    System.Console.Write("First Number:");
+                    string first = System.Console.ReadLine();
+                    System.Console.Write("Second Number:");
+                    string second = System.Console.ReadLine();
+                    Calculator<string> Operations = new Calculator<string>();
+                    double num, num2;
+                    bool firstnum = double.TryParse(first, out num);
+                    bool secondnum = double.TryParse(second, out num2);
+                    if (firstnum == true && secondnum == true)
+                    {
+                        switch (operation)
+                        {
+                            case "*": { Operation<double> del = Calculator<double>.Multiply<double>; double result = del.Invoke(num, num2); Console.WriteLine("result:" + result); } break;
+                            case "-": { Operation<double> del = Calculator<double>.Minus<double>; double result = del.Invoke(num, num2); Console.WriteLine("result:" + result); } break;
+                            case "/": { if (num2 != 0) { Operation<double> del = Calculator<double>.Divide<double>; double result = del.Invoke(num, num2); Console.WriteLine("result:" + result); } else { System.Console.WriteLine("Error"); } } break;
+                            case "+": { Operation<double> del = Calculator<double>.Plus<double>; double result = del.Invoke(num, num2); Console.WriteLine("result:" + result); } break;
+                        }
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        System.Console.WriteLine("Please write a number");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                }
+                else { work = false; }
             }
         }
     }
-class ArrayHelper<M>
-{
-    public M Pop(ref M[] ar)
+    class Calculator<T>
     {
-        M[] nar = new M[ar.Length - 1];
-        for (int x = 0; x < ar.Length - 1; x++)
+        public static T Plus<T>(T first, T second)
         {
-            nar[x] = ar[x];
+            return (dynamic)first + (dynamic)second;
         }
-        M last = ar[ar.Length - 1];
-        ar = nar;
-        return last;
-    }
-    public void Push(ref M[] ar, M ne)
-    {
-        M[] nar = new M[ar.Length + 1];
-        for (int x = 0; x < ar.Length; x++)
+        public static T Minus<T>(T first, T second)
         {
-            nar[x] = ar[x];
+            return (dynamic)first - (dynamic)second;
         }
-        nar[nar.Length - 1] = ne;
-        ar = nar;
-    }
-    public M Shift(ref M[] ar)
-    {
-        M[] nar = new M[ar.Length - 1];
-        for (int x = 0; x < nar.Length; x++)
+        public static T Multiply<T>(T first, T second)
         {
-            nar[x] = ar[x + 1];
+            return (dynamic)first * (dynamic)second;
+
         }
-        M first= ar[0];
-        ar = nar;
-        return first;
-    }
-    public void UnShift(ref M[] ar, M ne)
-    {
-        M[] nar = new M[ar.Length + 1];
-        nar[0] = ne;
-        for (int x = 0; x < nar.Length - 1; x++)
+        public static T Divide<T>(T first, T second)
         {
-            nar[x + 1] = ar[x];
-        }
-        ar = nar;
-    }
-    public M[] Slice(ref M[] arr, int x)
-    {
-        if (x < 0)
-        {
-            M[] narr = new M[Math.Abs(x)];
-            int cn = 0;
-            bool ch = true;
-            for (int i = arr.Length - 1; ch; i--)
-            {
-                ch = !(cn == Math.Abs(x) - 1);
-                narr[cn] = arr[i];
-                cn++;
-            }
-            return narr;
-        }
-        else if (x > 0)
-        {
-            M[] narr = new M[arr.Length - x + 1];
-            int cn = 0;
-            for (int i = 0; i <= x; i++)
-            {
-                narr[cn] = arr[i];
-                cn++;
-            }
-            return narr;
-        }
-        else
-        {
-            return arr;
+            return (dynamic)first / (dynamic)second;
+
         }
     }
-    public M[] Slice(ref M[] arr, int x, int y)
-    {
-        if (x > 0 && y > 0)
-        {
-            M[] narr = new M[y - x + 1];
-            int cn = 0;
-            for (int i = x; i <= y; i++)
-            {
-                narr[cn] = arr[i];
-                cn++;
-            }
-            return narr;
-        }
-        else if (x > 0 && y < 0)
-        {
-            M[] narr = new M[arr.Length - Math.Abs(y) - 1];
-            int cn = 0;
-            for (int i = x; i <= narr.Length; i++)
-            {
-                narr[cn] = arr[i];
-                cn++;
-            }
-            return narr;
-        }
-        else
-        {
-            return arr;
-        }
-    }
-}
 }
